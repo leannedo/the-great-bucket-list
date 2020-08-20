572f0c7cf620b900a5a3d955c382576ee02d6281
leannedo <ngocdo0912@gmail.com>
2020-08-20 22:38:26 +0300

    Implement progress bar and filter bar component

--- client/src/components/FilterBar/index.js
+++ client/src/components/FilterBar/index.js
@@ -1,12 +1,30 @@
 // Libraries
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 // Styling
 import "./FilterBar.scss";
 
 const FilterBar = ({ className }) => {
-  return <div className={className}>FilterBar</div>;
+  const [active, setActive] = useState("ALL");
+  const filterAction = ["ALL", "ONGOING", "COMPLETED"];
+
+  return (
+    <div className={`td-filter-bar-wrapper ${className}`}>
+      <div className="td-filter-bar-count">8 tasks left</div>
+      <div className="td-filter-bar-filter-wrapper">
+        {filterAction.map((action, id) => (
+          <div
+            key={id}
+            onClick={() => setActive(action)}
+            className={`filter ${active === action ? "active" : ""}`}
+          >
+            {action}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 FilterBar.defaultProps = {
--- client/src/components/ProgressBar/index.js
+++ client/src/components/ProgressBar/index.js
@@ -5,16 +5,32 @@ import PropTypes from "prop-types";
 // Styling
 import "./ProgressBar.scss";
 
-const ProgressBar = ({ className }) => {
-  return <div className={className}>Progress Bar</div>;
+const ProgressBar = ({ className, progress }) => {
+  const style = { position: "absolute", right: "20px" };
+  return (
+    <div className={`td-progress-bar-wrapper ${className}`}>
+      <div
+        style={{ width: `${progress.toString()}%` }}
+        className="td-progress-fill"
+      />
+      <div
+        className="td-progress-text-wrapper"
+        style={progress >= 70 ? style : {}}
+      >
+        <div className="td-progress-text">{`${progress.toString()}% Completed`}</div>
+      </div>
+    </div>
+  );
 };
 
 ProgressBar.defaultProps = {
   className: "",
+  progress: 0,
 };
 
 ProgressBar.propTypes = {
   className: PropTypes.string,
+  progress: PropTypes.number,
 };
 
 export default ProgressBar;
