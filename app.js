d716396b5c0f8b632a9f495809b1aadea8fb94d2
leannedo <ngocdo0912@gmail.com>
2020-08-26 23:55:54 +0300

    Connecting server to serve build

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const  express    = require('express'),
        app        = express(),
        port       = process.env.PORT || 3000,
-       bodyParser = require('body-parser');
+       bodyParser = require('body-parser'),
+       path = require('path');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/', (req, res) => res.send('Welcome to todo app'));
+const publicPath = path.join(__dirname, '/client', 'build');
+
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 app.listen(port, () => console.log(`Server is running on ${port}`));
