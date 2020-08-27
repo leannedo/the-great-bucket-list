d916677e35e9948004f76af19c8bf46e95add5c6
leannedo <ngocdo0912@gmail.com>
2020-08-28 02:45:46 +0300

    Add more test

---
+++ client/src/components/ProgressBar/__tests__/ProgressBar.spec.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { mount } from "enzyme";
+
+import ProgressBar from "../index";
+
+describe("ProgressBar render", () => {
+  it("renders correct progress text", () => {
+    const wrapper = mount(<ProgressBar progress={"75"} />);
+
+    expect(
+      wrapper.contains(<div className="td-progress-text">75% Completed</div>)
+    ).toBeTruthy();
+  });
+});
