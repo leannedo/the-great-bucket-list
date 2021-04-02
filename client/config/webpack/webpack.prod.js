d422156ad1fc5d7235bf2285180cdd0f4bb699be
leannedo <ngocdo0912@gmail.com>
2021-04-02 21:14:32 +0300

    fix public path

--- client/config/webpack/webpack.prod.js
+++ client/config/webpack/webpack.prod.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: 'source-map',
   output: {
     filename: 'js/[name].[contenthash].bundle.js',
-    publicPath: '/home',
+    publicPath: '/',
   },
   plugins: [
     // style-loader is for development, MiniCssExtractPlugin is for production
