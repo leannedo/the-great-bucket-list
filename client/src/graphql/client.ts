cfb0e93d0e866e16a3db735213443f896f77bf22
ngocdo <ngocdo0912@gmail.com>
2021-04-20 00:05:24 +0300

    update BE url

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -2,6 +2,6 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const createClient = (): ApolloClient<unknown> =>
   new ApolloClient({
-    uri: 'http://localhost:5000/graphql',
+    uri: 'https://the-great-bucket-list-be.herokuapp.com/graphql',
     cache: new InMemoryCache(),
   });
